45caa3bd984f7af5b3e01a6afa72f6c1648a2442
Divya Manirajan <divyamanirajan@divyas-macbook-pro.local>
2018-10-29 11:23:11 -0400

    Commit

--- PuppyGame/ViewController.swift
+++ PuppyGame/ViewController.swift
@@ -9,10 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var points = 0
+    
+    @IBOutlet weak var pup: UIImageView!
+    
+    @IBOutlet weak var bone: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    @IBAction func handlePan(recognizer:UIPanGestureRecognizer) {
+        let translation = recognizer.translation(in: self.view)
+        if let view = recognizer.view {
+            view.center = CGPoint(x:view.center.x + translation.x,
+                                  y:view.center.y + translation.y)
+        }
+        recognizer.setTranslation(CGPoint.zero, in: self.view)
+        while(points <= 10){
+            var x = arc4random_uniform(375)+1
+            var y = arc4random_uniform(667)+1
+            if(pup.center == bone.center){
+                
+            }
+        }
+        //location
+        //if loop
     }
 
 
