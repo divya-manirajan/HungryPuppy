3b9be5aa6d39d24440b8a1305e6bcfb58452f63b
Divya Manirajan <divyamanirajan@divyas-macbook-pro.local>
2018-10-29 10:36:53 -0400

    Initial Commit

---
+++ PuppyGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PuppyGame
+//
+//  Created by Divya Manirajan on 10/29/18.
+//  Copyright © 2018 Divya Manirajan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
